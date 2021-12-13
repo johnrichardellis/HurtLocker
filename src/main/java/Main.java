8e28ab5e96b1691de3f991a1d93ef3a03eeada81
Ellis John <ellisjohn@zipcodes-MacBook-Pro-chuck.local>
2021-12-13 11:20:24 -0500

    replacing symbols with colons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,33 +3,69 @@ import org.apache.commons.io.IOUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public String readRawDataToString(){
 
+//        replaceAllSymbolsWithColons();
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("RawData.txt").getFile());
-        StringBuilder result = new StringBuilder("");
+        StringBuilder result = new StringBuilder(""); // empty result sb
 
+        try(Scanner scanner = new Scanner(file)) { // instance of new scanner
 
-
-        try(Scanner scanner = new Scanner(file)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                result.append(line).append("\n");
+            while (scanner.hasNextLine()) { // keep using scanner while there is a next line
+//                String line = scanner.nextLine(); //
+                result.append(scanner.nextLine()).append("\n"); // add the line and new line to result
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) { // might throw exception
+            e.printStackTrace(); // print stack trace if so
         }
+//        System.out.println(result.toString());
+        return result.toString(); // convert sb to string and return
 
-        return result.toString();
     }
+
+
+
+    public String replaceAllSymbolsWithColons() {  // used to change all symbols to colon instead
+
+        try {
+            String result = "";
+            String groceryList = readRawDataToString();
+            Pattern pat = Pattern.compile("[;#^*%]"); // replace these symbols
+            Matcher mat = pat.matcher(groceryList); // in our groceryList
+            result = result + mat.replaceAll(":"); // with the colon
+            System.out.println(result);
+            return result;
+        } catch(Exception e) {
+            throw new UnsupportedOperationException();
+        }
+//        System.out.println(result);
+    }
+
+
+//    public String correctNames()
+
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
