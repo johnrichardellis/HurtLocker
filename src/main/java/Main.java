ea29d3d8f29b43f4c653a6dadfc9bced30a49771
Ellis John <ellisjohn@zipcodes-MacBook-Pro-chuck.local>
2021-12-13 09:47:31 -0500

    setting up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,30 @@
 import org.apache.commons.io.IOUtils;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public String readRawDataToString() throws Exception{
+    public String readRawDataToString(){
+
         ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-        return result;
+        File file = new File(classLoader.getResource("RawData.txt").getFile());
+        StringBuilder result = new StringBuilder("");
+
+
+
+        try(Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) throws Exception{
